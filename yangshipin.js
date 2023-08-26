ea8a84366b7aefd61c19ded846a77a0cf89eb3ba
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-08-26 15:06:42 +0800

    Update yangshipin.js

--- yangshipin.js
+++ yangshipin.js
@@ -13,8 +13,8 @@
 &guid=\w+&app_version=([\d\.]+)&spadseg=(\d)&userid=(|\d+) url 302 &guid=26ae4779b2b411ec90a46c92bfd79530&app_version=$1&spadseg=$2&vuserid=209027229
 #^https:\/\/liveinfo\.ysp\.cctv\.cn\/ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/yangshipin.js
 #^https:\/\/m\.yangshipin\.cn\/static\/\w/\w+\/index\.html$ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/yangshipin.js
-# > 央视频 去广告来源 @广告必须死
-^https?:\/\/cdn\.cmgadx\.com\/sdk\/pool\/m8uTS50pt3DC0Xd6\.json url reject-200
+# > 央视频 去广告 据说解锁1080 自测
+^https?:\/\/cdn\.cmgadx\.com\/sdk\/pool\/.+\.json url reject-dict
 
 
 [mitm]
