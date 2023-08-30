4f9268fc37ff0c1e0a7ca8a7e037110541b87c1d
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-08-30 14:05:07 +0800

    Create Anycats.js

---
+++ Anycats.js
@@ -0,0 +1,23 @@
+/*
+项目名称：Anycats
+下载地址：美区商店 测试版本2.5
+TG:@ios151 2023.8.30.13.58
+
+
+
+[rewrite_local]
+^https?:\/\/api\.(tuanleme|fengyunyizu|jianghumeng)\.(net|com)\/ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Anycats.js
+
+
+[mitm]
+hostname = api.tuanleme.net, api.fengyunyizu.com, api.jianghumeng.net
+
+
+
+
+var objc = JSON.parse($response.body);
+
+objc.data.account.expiration_date = 1698903118878734;
+objc.data.account.has_password = true;
+
+$done({ body: JSON.stringify(objc) });
