cffa776d2f7fd5dc472dd68f70a619b2a7ee5840
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-08-31 18:32:57 +0800

    Update xiangjiyin.js

--- xiangjiyin.js
+++ xiangjiyin.js
@@ -8,7 +8,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body xiangjiyin.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/xiangjiyin.js
 
 [mitm] 
 hostname = api.revenuecat.com
