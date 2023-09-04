82e6d7c3f0a399e7e646dbd954834183fc266c54
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-09-04 13:54:30 +0800

    Create lygq.js

---
+++ lygq.js
@@ -0,0 +1,23 @@
+/*************************************
+
+项目名称：来音钢琴
+软件版本：4.3.0
+下载地址：https://is.gd/1LiXln
+脚本作者：安妮
+使用声明：⚠️仅供参考，🈲️转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/piano-api\.quthing\.com\/vip\/state\/v2 url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/lygq.js
+
+[mitm]
+hostname = piano-api.quthing.com
+
+
+*************************************/
+
+
+
+
+
